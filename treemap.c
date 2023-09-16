c99f0492f590566b4474a3c83efed478fe8f3363
Replit user <>
2023-09-16 00:30:52 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -36,11 +36,10 @@ TreeNode * createTreeNode(void* key, void * value) {
     return new;
 }
 
-/* Esta función recibe la función de comparación de claves y crea un mapa (TreeMap) inicializando sus variables. 
-El siguiente código muestra como inicializar la función de comparación. Reserve memoria, inicialice el resto de variables y retorne el mapa. */
+/* Esta función recibe la función de comparación de claves y se crea un mapa para el árbol con su respectiva asignacion de memoria e inicializacion de cada componente de este. Se retorna el mapa creado.*/
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
     TreeMap * map = (TreeMap *) malloc(sizeof(TreeMap));
-    if(map == NULL) exit(EXIT_FAILURE);
+    if(map == NULL) exit(EXIT_FAILURE); // asignacion de memoria fallida
   
     map->root = NULL;
     map->current = NULL;
@@ -48,8 +47,10 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
     return map;
 }
 
-/* inserta un nuevo dato (key,value) en el árbol y hace que el current apunte al nuevo nodo.
-Para insertar un dato, primero debe realizar una búsqueda para encontrar donde debería ubicarse. Luego crear el nuevo nodo y enlazarlo. Si la clave del dato ya existe retorne sin hacer nada (recuerde que el mapa no permite claves repetidas). */
+/* Esta funcion inserta un nuevo dato (key,value) en el árbol y hace que el current apunte al nuevo nodo. Para esto, se  crea un nuevo nodo y se le asigna memoria para el par clave-valor y para los punteros izquierdo, derecho y padre.
+--- Condiciones de busqueda: ---
+- Si el árbol está vacío (sin raíz), se establece nodo como la raíz del árbol y se sale de la función.
+- Si el árbol no está vacío, y si la clave ya existe en el árbol, no se inserta. El nuevo nodo se coloca a la izquierda si la clave es menor que el nodo actual, y a la derecha si es mayor.*/
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
     TreeNode * nodo = (TreeNode *) malloc(sizeof(TreeNode));
     if(nodo == NULL) exit(EXIT_FAILURE);
@@ -61,6 +62,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     nodo->right = NULL;
     nodo->parent = NULL;
 
+    // arbol vacío
     if(tree->root == NULL) {
       tree->root = nodo;
       return;
@@ -98,7 +100,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     }
 }
 
-/* retorna el **nodo con la mínima clave** ubicado en el subárbol con raiz x. Para obtener el nodo tiene que, a partir del nodo x, irse por la rama izquierda hasta llegar al final del subárbol. Si x no tiene hijo izquierdo se retorna el mismo nodo. */
+/* Esta función encuentra el nodo con la clave más pequeña en un árbol binario de búsqueda a partir de un nodo dado x siguiendo la rama izquierda del árbol hasta que no haya más hijos izquierdos. Luego devuelve ese nodo. Si x no tiene hijo izquierdo se retorna el mismo nodo. */
 TreeNode * minimum(TreeNode * x){
     TreeNode * current = x;
     if(current->left == NULL) {
@@ -110,10 +112,11 @@ TreeNode * minimum(TreeNode * x){
   return current;
 }
 
-/* elimina el nodo *node* del árbol *tree*. Recuerde que para eliminar un node existen 3 casos:
-**Nodo sin hijos:** Se anula el puntero del padre que apuntaba al nodo
-**Nodo con un hijo:** El padre del nodo pasa a ser padre de su hijo
-**Nodo con dos hijos:** Descienda al hijo derecho y obtenga el menor nodo del subárbol (con la función minimum). Reemplace los datos (key,value) de *node* con los del nodo "minimum". Elimine el nodo minimum (para hacerlo puede usar la misma función *removeNode*). */
+/* Esta función elimina un nodo de un arbol binario de busqueda. 
+--- Existen 3 casos: ---
+- nodo sin hijos Se anula el puntero del padre que apuntaba al nodo.
+- nodo con un hijo: El padre del nodo pasa a ser padre de su hijo.
+- nodo con dos hijos: Se encuentra el sucesor (el nodo con la clave más pequeña en el subárbol derecho) con la funcion minimum y se copian sus valores al nodo actual. Luego se elimina el sucesor llamando a removeNode recursivamente. */
 void removeNode(TreeMap * tree, TreeNode* node) {
     if (tree == NULL || node == NULL) return;
   
@@ -186,44 +189,53 @@ void eraseTreeMap(TreeMap * tree, void* key) {
 
 }
 
-/* busca el nodo con clave igual a key y retorna el **Pair** asociado al nodo. Si no se encuentra la clave retorna NULL.
-Recuerde hacer que el current apunte al nodo encontrado. */
-/* int resultado = map->lower_than(key1,key2);
-Esta función retorna 1 si key1<key2. */
-/* int is_equal(TreeMap* tree, void* key1, void* key2);
-Retorna 1 si las claves son iguales y 0 si no lo son. */
+/* Esta funcion busca el nodo con clave igual a key desde la raiz hasta llegar a una hoja o encontrar una coincidencia con la clave buscada.
+--- Comparaciones en la busqueda: ---
+- Si las claves son iguales, se devuelve el par asociado con ese nodo.
+- Si la clave buscada es menor que la clave del nodo actual, se mueve hacia la izquierda explorando el subárbol izquierdo actualizando el puntero a current.
+- Si la clave buscada es mayor que la clave del nodo actual, se mueve hacia la derecha explorando el subárbol derecho actualizando el puntero a current.
+- Si no se encuentra un nodo con la clave buscada, la función devuelve NULL*/
 Pair * searchTreeMap(TreeMap * tree, void* key) {
     tree->current = tree->root;
     while(tree->current != NULL) {
+      // misma clave
       if(is_equal(tree, key, tree->current->pair->key) == 1) {
         return tree->current->pair;
       }
+      // key es menor que la clave del nodo
       else if(tree->lower_than(key, tree->current->pair->key) == 1) {
         tree->current = tree->current->left;
       }
+      // key es mayor que la clave del nodo
       else {
         tree->current = tree->current->right;
       }
     }
+    // no se encontró la clave
     return NULL;
 }
 
-/* retorna el **Pair** con clave igual a key. En caso de no encontrarlo retorna el primer par asociado a una clave mayor o igual a key. 
-Para implementarla puede realizar una búsqueda normal y usar un puntero a nodo auxiliar ub_node que vaya guardando el nodo con la menor clave *mayor o igual a key*. Finalmente retorne el par del nodo ub\_node. */
+/* Esta función busca el par clave-valor más cercano al valor dado (key) que sea mayor o igual en valor y se retorna el par clave-valor asociado a ub_node. Comienza desde la raíz hasta que el nodo temp sea NULL y temp tenga un par diferente de NULL (de lo contrario se rompe el ciclo).
+--- Comparaciones en la busqueda: ---
+- Si la clave dada es menor que la clave del nodo actual, se mueve el puntero temp hacia la izquierda explorando el subárbol izquierdo.
+- Si la clave del nodo actual es menor que la clave dada, se mueve el puntero temp hacia la derecha explorando el subárbol derecho.
+- Si ninguna de las condiciones anteriores se cumple, significa que se encontró un nodo cuya clave es igual a la clave dada y se retorna el par clave-valor asociado a ese nodo. */
 Pair * upperBound(TreeMap * tree, void* key) {
   TreeNode* ub_node = NULL;
   TreeNode* temp = tree->root;
 
   while(temp != NULL) {
     if(temp->pair != NULL) {
+      // key es menor que la clave del nodo
       if(tree->lower_than(key, temp->pair->key) == 1) {
       ub_node = temp;
       temp = temp->left;
       }
+      // la clave del nodo es menor que key
       else if(tree->lower_than(temp->pair->key, key) == 1) {
         temp = temp->right;
       }
-      // clave = clave del nodo
+      // misma clave
       else {
         return temp->pair;
       }
@@ -232,7 +244,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
       break;
     }
   }
-  // Null si todas las claves son menores
+  // se retorna NULL si todas las claves del arbol son menores
   if(ub_node != NULL) {
     return ub_node->pair;
   }
@@ -241,8 +253,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
   }
 }
 
-/* Pair* firstTreeMap(TreeMap* tree) retorna el primer **Pair** del mapa (el menor).
-Pair* nextTreeMap(TreeMap* tree)  retornar el siguiente **Pair** del mapa a partir del puntero TreeNode* current. Recuerde actualizar este puntero. */
+/* Esta funcion retorna el primer par clave-valor más pequeño del mapa y actualiza el puntero current del árbol para apuntar a ese nodo. */
 Pair * firstTreeMap(TreeMap * tree) {
     if(tree->root == NULL) return NULL;
     TreeNode * minimo = minimum(tree->root);
@@ -250,7 +261,11 @@ Pair * firstTreeMap(TreeMap * tree) {
     return minimo->pair;
 }
 
-/* NEXT se refiere al valor más cercano al current, por ejemplo 12->13 en el subarbol derecho*/
+/* Esta funcion busca el siguiente par clave-valor en orden después del nodo actual current.
+--- Comparaciones de busqueda: ---
+- Si hay un subárbol derecho, se actualiza el puntero current al nodo mínimo en ese subárbol derecho utilizando la función minimum y luego retorna el par clave-valor de ese nodo mínimo.
+- Si no hay subárbol derecho (es un nodo hoja o un nodo interno sin hijo derecho), se retrocede a través de los ancestros hasta encontrar un nodo que sea hijo izquierdo de su padre (o hasta que no hayan mas ancestros), se actualiza el current y retorna su par clave-valor.
+- Si llega a la raíz sin encontrar un nodo adecuado retorna NULL (no hay un siguiente nodo en orden). */
 Pair * nextTreeMap(TreeMap * tree) {
     if(tree->root == NULL || tree->current == NULL) return NULL;
 
@@ -260,7 +275,7 @@ Pair * nextTreeMap(TreeMap * tree) {
       return tree->current->pair;
     }
     // ancestro al current en el subarbol derecho (no tiene hijo derecho)
-    while (tree->current->parent != NULL && tree->current == tree->current->parent->right) {
+    while (tree->current->parent != NULL && tree->current == tree->current->parent->right) { // (busqueda del ancestro)
         tree->current = tree->current->parent;
     }
     if (tree->current->parent != NULL) {
